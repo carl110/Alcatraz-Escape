2a81219af53cd545d005492dd18918390bf3aed3
Carl Wainwright <carljame7@gmail.com>
2020-01-23 12:02:10 +0000

    Initial Commit

---
+++ Alcatraz EscapeTests/Alcatraz_EscapeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Alcatraz_EscapeTests.swift
+//  Alcatraz EscapeTests
+//
+//  Created by Carl Wainwright on 23/01/2020.
+//  Copyright © 2020 Carl Wainwright. All rights reserved.
+//
+
+import XCTest
+@testable import Alcatraz_Escape
+
+class Alcatraz_EscapeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
